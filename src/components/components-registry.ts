51b3d8305440456b82bc01abf7cabb340b63523f
Simon Hanukaev <semenh@gmail.com>
2021-12-02 01:21:46 +0200

    update jsdocs in components-registry.ts

--- src/components/components-registry.ts
+++ src/components/components-registry.ts
@@ -2,28 +2,29 @@ import dynamic from 'next/dynamic';
 import { ComponentType } from 'react';
 
 /**
- * The getComponent() function imports dynamic component.
+ * The getComponent() function loads a component using dynamic import.
  *
- * Dynamic components are loaded by the browser only when these components are actually used.
- * For example, a "HeroSection" component, and its dependencies will, will not be loaded by the browser unless the code
- * call getComponent('HeroSection'),
+ * Dynamic imports are useful when you wish to load a module conditionally. For example, if a home page renders the
+ * "HeroSection" conditionally, then loading it with getComponent('HeroSection') will ensure that the "HeroSection"
+ * is bundled only when used.
  */
 export function getComponent(key: string): ComponentType {
     return components[key];
 }
 
 /**
- * Map of dynamic components.
+ * Map of dynamically imported components.
  *
- * The mapping key of a dynamic component is a model name, and the value is the component imported via Next's
- * dynamic import. You should use dynamic components for large components or components with heavy external dependencies,
- * which are used sparingly in your website's pages. To learn more about Nextjs dynamic imports visit:
+ * The mapping key of a dynamically imported component is the model name describing the props of that component.
+ * And the value is the component imported via Next.js dynamic import. You should use dynamic components for large
+ * components or components with heavy external dependencies that are used sparingly in your website's pages.
+ * To learn more about Nextjs dynamic imports visit:
  * https://nextjs.org/docs/advanced-features/dynamic-import
  *
- * Dynamic components are mapped by model names that describe the prop types of these components. This fact allows
- * selecting a dynamic component at run-time based on the type of the content, and the type of the content will usually
- * be the name of the content's model. For example, a parent component calls the `getComponent` function, passing it the
- * type of data it needs to render, and gets back the registered component for that type of data:
+ * Dynamic components can be selected at run-time based on the type of their content (props). This is because
+ * components are mapped by models that describe their content, and content's type always matches the model name.
+ * For example, a page component can call `getComponent(section.type)` function, passing it the type of section
+ * data it needs to render, and get back the component that can render that type of data:
  *
  *     const Section = getComponent(section.type);
  *     return <Section {...section} />;
