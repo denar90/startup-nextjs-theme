74f6b6bf6d8e682a12118595fe034d1852e3e269
tomasbankauskas <tbdizainas@gmail.com>
2021-12-05 22:16:53 +0200

    updated footer

--- src/components/sections/Footer/index.tsx
+++ src/components/sections/Footer/index.tsx
@@ -20,16 +20,12 @@ export default function Footer(props) {
             <div className="sb-divider" />
                 {(props.logo || props.title || props.text) && (
                     <div className="mb-12 mt-6">
-                        {props.logo && (
-                            <Link href="/" className="inline-block mb-4" data-sb-field-path=".logo">
-                                {props.logo && <ImageBlock {...props.logo} className="max-h-12" />}
+
+                            <Link href="/" className="flex items-center" data-sb-field-path=".title#span[1] .logo#img[1]">
+                                {props.logo && <ImageBlock {...props.logo} className={classNames('max-h-12', { 'mr-2': props.title })} />}
+                                {props.title && <span className="text-2xl tracking-wide">{props.title}</span>}
                             </Link>
-                        )}
-                        {props.title && (
-                            <div className="mb-2 text-2xl tracking-wide" data-sb-field-path=".title">
-                                {props.title}
-                            </div>
-                        )}
+
                         {props.text && (
                             <Markdown options={{ forceBlock: true, forceWrapper: true }} className="sb-markdown max-w-xl" data-sb-field-path=".text">
                                 {props.text}
