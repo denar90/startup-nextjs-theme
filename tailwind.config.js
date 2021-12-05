81cd88ce841da618a71f6a2eac5acb5719a91e44
TomasBankauskas <tbdizainas@gmail.com>
2021-12-05 20:36:04 +0200

    blockquote style updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,7 +47,7 @@ module.exports = {
                 body: {
                     fontFamily: theme(`fontFamily.${themeStyle.fontBody}`)
                 },
-                'h1,h2,h3,h4,h5,h6': {
+                'h1,h2,h3,h4,h5,h6,blockquote': {
                     fontFamily: theme(`fontFamily.${themeStyle.fontHeadlines}`)
                 },
                 h1: {
